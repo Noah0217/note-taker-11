40accfd4b026d71424cc2575d77d8b2b5392ee44
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-25 10:03:12 -0600

    added server dependencies

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
 //dependencies
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const notes = require("./Develop/db/db.json");
+
+//server dependencies 
+const app = express();
+const PORT = process.env.PORT
