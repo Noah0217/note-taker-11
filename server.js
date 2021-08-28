92215ab9f15da884f65e5c4256f34a3eeb96f369
Noah Mejia <84366215+Noah0217@users.noreply.github.com>
2021-08-28 14:33:22 -0600

    added note save and delete function

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const notes = require('./db/db.json'); 
+const database = require('./db/db.json'); 
 
 //path
 const mainPath = path.join(__dirname, './public') 
@@ -16,66 +16,66 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 app.use(express.json());
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+})
 
-//route to html notes
-app.get('/notes', function(req, res) {
-    res.sendFile(path.join(mainPath, 'notes.html'));
-});
-app.get('/api/notes', function(req, res) {
-    res.sendFile(path.join(db, 'db.json'))
-    return res.body
-});
-app.post('/api/notes', function(req, res) {
-    console.log(req.body)
-    console.log("post route hit")
-    res.sendFile(path.join(db, 'db.json'))
-    return res.body
-});
-app.get("*", function(req, res) {
-    res.sendFile(path.join(mainPath, 'index.html'));
-});
-
-//Read and write file 
+//save note
 app.route("/api/notes")
-    // Grab the notes list
     .get(function (req, res) {
-        res.json(notes);
+        res.json(database);
     })
 
-.post(function (req, res) {
-    let jsonFilePath = path.join(__dirname, "/db/db.json");
-    let newNote = req.body;
+    // Add a new note to the json db file.
+    .post(function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        let newNote = req.body;
+        let highestId = 99;
+        for (let i = 0; i < database.length; i++) {
+            let individualNote = database[i];
 
-    // This allows the test note to be the original note.
-    let highestId = 99;
-    // This loops through the array and finds the highest ID.
-    for (let i = 0; i < notes.length; i++) {
-        let individualNote = notes[i];
+            if (individualNote.id > highestId) {
+                highestId = individualNote.id;
+            }
+        } 
+        newNote.id = highestId + 1;
+        database.push(newNote)
 
-        if (individualNote.id > highestId) {
-            // highestId will always be the highest numbered id in the notesArray.
-            highestId = individualNote.id;
-        }
-    }
-    // This assigns an ID to the newNote. 
-    newNote.id = highestId + 1;
-    // We push it to db.json.
-    notes.push(newNote)
+        fs.writeFile(jsonFilePath, JSON.stringify(database), function (err) {
 
-    // Write the db.json file again.
-    fs.writeFile(jsonFilePath, JSON.stringify(notes), function (err) {
-
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Your note was saved!");
+            if (err) {
+                return console.log(err);
+            }
+            console.log("Your note was saved!");
+        }); 
+        res.json(newNote);
     });
-    // Gives back the response, which is the user's new note. 
-    res.json(newNote);
-});
 
+    //delete note
+    app.delete("/api/notes/:id", function (req, res) {
+        let jsonFilePath = path.join(__dirname, "/db/db.json");
+        for (let i = 0; i < database.length; i++) {
+    
+            if (database[i].id == req.params.id) {
+                database.splice(i, 1);
+                break;
+            }
+        }
+        // Write to db file
+        fs.writeFileSync(jsonFilePath, JSON.stringify(database), function (err) {
+    
+            if (err) {
+                return console.log(err);
+            } else {
+                console.log("Your note was deleted!");
+            }
+        });
+        res.json(database);
+    });
 
 //successfully connected to port
 app.listen(PORT, () => 
 console.log(`successfully connected to http://localhost:${PORT}`)); //https://stackoverflow.com/questions/37929173/significance-of-port-3000-in-express-apps
-
